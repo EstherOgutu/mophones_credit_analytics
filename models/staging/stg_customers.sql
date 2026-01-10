b2deafff2225f71244b90df3c4dc37f1d0d92456
Esther Ogutu <estherogutu@Esthers-MacBook-Air.local>
2026-01-10 12:28:36 +0300

    feat: integrate nps survey data and create credit-sentiment analysis

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -15,7 +15,8 @@ SELECT
   sales.loan_price,
   sales.product_name,
   sales.loan_term,
-  sales.returned                           AS is_returned
+  sales.returned                           AS is_returned,
+  nps."Using a scale from 0 (not likely) to 10 (very likely), how likely are you to recommend MoPhones to friends or family?" AS nps_score
 FROM {{ ref('seed_customer_income') }} AS income
 LEFT JOIN {{ ref('seed_customer_dob') }} AS dob ON
   income."Loan Id" = dob."Loan Id "
@@ -23,5 +24,7 @@ LEFT JOIN {{ ref('seed_customer_gender') }} AS gender ON
   income."Loan Id" = gender."Loan Id"
 LEFT JOIN {{ ref('seed_sales_data') }} AS sales ON
   income."Loan Id" = sales."Loan Id"
+LEFT JOIN {{ ref('seed_nps_data') }} AS nps ON
+  income."Loan Id" = nps."Loan Id"
 WHERE 
   income."Loan Id" IS NOT NULL
